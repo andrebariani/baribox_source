b528cc52c538f0cc2dfbace75af996f1dda3799a
andrebariani <andrebariani@gmail.com>
2024-03-28 17:12:32 -0300

    adjusts to text

--- editor/EditorConfig.ts
+++ editor/EditorConfig.ts
@@ -23,8 +23,8 @@ export function prettyNumber(value: number): string {
 }
 
 export class EditorConfig {
-    public static readonly version: string = "2.2.15"; // Currently using patch versions in display (unlike JB)
-    public static readonly versionDisplayName: string = "UltraBox " + EditorConfig.version;
+    public static readonly version: string = "2.2.15"; // Using patch versions in display right now, maybe TODAY.
+    public static readonly versionDisplayName: string = "BariBox™ " + EditorConfig.version;
 
     public static readonly releaseNotesURL: string = "./patch_notes.html";
 
